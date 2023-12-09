8553339d43865347ae110545d97b773d7c38fa48
annaZakharchenko <144456200+annaZakharchenko@users.noreply.github.com>
2023-12-09 23:40:59 +0100

    Delete Header.h

--- Header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-void heapS(vector<int>& arr, int size, int i);
-void heapSort(vector<int>& arr, int size);
-void quickSort(vector<int>& numbers, int left, int right);
